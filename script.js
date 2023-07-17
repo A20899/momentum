14334ddc409c91eb72ac38b09c18f5c68eec7982
Amiel <amieldatuin23@gmail.com>
2023-07-17 22:51:09 +0800

    Add clock and quotes

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ const greetingElement = document.getElementById('greeting');
 
 function updateTime() {
   const date = new Date();
-  const hours = date.getHours();
+  let hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
+  let is12 = localStorage.getItem("clockContext") || "12"
+  let session = is12 == "12" ? "AM" : "";
 
-  const timeString = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  if (is12 == "12") {
+    if(hours === 0){ 
+        hours = 12;
+    }
+    if(hours > 12){
+        hours = hours - 12;
+        session = "PM";
+     }
+    }
+  
+
+  const timeString = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${session}`;
   timeElement.textContent = timeString;
-
+    
   setTimeout(updateTime, 1000);
 }
 
@@ -31,6 +44,10 @@ function setGreeting() {
   greetingElement.textContent = greeting;
 }
 
+timeElement.addEventListener("click", () => {
+  const storage = localStorage.getItem("clockContext") || "12"
+  localStorage.setItem("clockContext", storage == "12" ? "24" : "12")
+})
 
 
 updateTime();
