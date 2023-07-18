0a2f4bf15c67b30af40bbd2af00c8bfa7e143e6e
Rich Yuson <106722744+RjYuson@users.noreply.github.com>
2023-07-18 11:07:16 +0800

    Add files via upload

---
+++ Momentum 2/js/background.js
@@ -0,0 +1,21 @@
+const background = document.querySelectorAll('.background')
+
+let imgIndex = 0;
+
+function changeBackground(){
+    // Remove .showing class from current background
+    background[imgIndex].classList.remove('showing');
+
+    // Increment of imgIndex so it jumps to the next background
+    imgIndex++;
+
+    // If the counter reaches the last element/background, set it to 0 to start from the first background again.
+    if(imgIndex >= background.length){
+        imgIndex = 0;
+    }
+
+    // Add the .showing class to the next background
+    background[imgIndex].classList.add('showing');
+}
+
+setInterval(changeBackground, 1000*60);
